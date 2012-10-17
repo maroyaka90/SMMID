b4311a9a0a197e867fca912c04272161c2fa63f4
smmid administrative user <smmid@smid-db.sgn.cornell.edu>
2012-10-17 15:28:18 -0400

    adding a simplified startup script.

---
+++ run_smmid_server.sh
@@ -0,0 +1,3 @@
+export PERL5LIB="/home/mueller/cxgn/cxgn-corelibs/lib:/home/mueller/cxgn/sgn/lib:/home/mueller/cxgn/tomato_genome/lib:/home/mueller/cxgn/Cview/lib:/home/mueller/cxgn/ITAG/lib:/home/mueller/cxgn/local-lib/lib:/home/mueller/cxgn/Phenome/lib:/home/mueller/cxgn/biosource/lib:/home/mueller/cxgn/local-lib/lib/perl5:$PERL5LIB"
+
+screen script/smmid_server.pl -r --fork
