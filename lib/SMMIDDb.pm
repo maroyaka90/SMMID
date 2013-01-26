36bfa9de01ec76ed6db96df464ff60e764aa33d4
Joshua Judkins <jj388@cornell.edu>
2013-01-26 15:35:30 -0500

    Revert "fixed error in organism retrieval"
    This reverts commit 3640bb887731b8abe383f3d0ad17410674181345.

--- lib/SMMIDDb.pm
+++ lib/SMMIDDb.pm
@@ -122,7 +122,7 @@ sub get_organsism {
   return $SMMID{$self->get_smmid()}->{"ORGANISM"};
 }
 
-sub set_organism {
+sub set_molecular_weight {
   my $self = shift;
   $SMMID{$self->get_smmid()}->{"ORGANISM"}=shift;
 }
