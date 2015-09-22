b55e620442f4e914b2ab8e6d9b95271a14a670a2
smmid administrative user <smmid@smid-db.sgn.cornell.edu>
2015-09-22 08:51:20 -0400

    fix perl library path.

--- run_smmid_server.sh
+++ run_smmid_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PERL5LIB=/usr/local/share/smid-db/local-lib/lib/perl5
-
+#export PERL5LIB=/usr/local/share/smid-db/local-lib/lib/perl5
+export PERL5LIB=/home/smmid/perl5/lib/perl5
 screen ./script/smmid_server.pl -r --fork
